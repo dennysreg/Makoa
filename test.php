43a520fd0aad34227fe3b5a1b4c3a9bf81df3426
dennys <dennys@dell-laptop.(none)>
2010-02-20 17:53:24 -0600

    plantilla de tweet ofrezco/necesito: mensaje @usuario_twitter actualizado

--- test.php
+++ test.php
@@ -1,17 +1,17 @@
 <?php
-
 //actualizar estado en twitter utilizando twitterlibphp http://jdp.github.com/twitterlibphp/doc/
 require "twitter.lib.php";
+//datos del usuario
 $username = "makoamx";
 $pass = "makoa123";
-echo "ok";
-
 //inicializa la clase de twitter
 $twitter = new Twitter($username, $pass);
-$estado =  $twitter->updateStatus ("plantilla @user necesito/ofrezco algo");
- 
-
-
-
-
+//datos de prueba
+$necesito_ofrezco = "Ofrezco";
+$mensaje = "Programación en Ruby on Rails";
+$usuario = "@usuario_twitter";
+///mensaje compuesto
+$mensaje_compuesto = $necesito_ofrezco.": ".$mensaje." ".$usuario;
+//actualizamos el estado
+$estado =  $twitter->updateStatus ($mensaje_compuesto);
 ?>
