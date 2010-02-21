7537095a7be49098156b873f4ec77c12d4b51ce7
Raúl Pineda <raul@raul-karmic-vm.(none)>
2010-02-20 18:45:28 -0600

    Se agrego la view de ver entrada

---
+++ viewEntry.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> 
+  <head>
+	<link rel="stylesheet" type="text/css" href="stylesheets/style.css" />
+  </head>
+  <body>
+	<div id="header"><img src="images/header.png" alt="Makoa.mx" title="Makoa.mx" /></div><!--header.png es una imagen del logo que aún no está hecho-->
+	<div id="wrapper">
+		<div id="userInfo">Bienvenido @shachiel | <a class="logout" href="#">Cerrar Sesi&oacute;n</a></div> <!--@shachiel debe ser el username de twitter -->
+		<div id="posterInfo">
+			<img src="" alt="" title="" />
+			<h2>@Shachiel</h2>
+		</div>
+		<h1 class="postTitle">T&iacute;tulo de la entrada</h1>
+		<div class="postContent">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin aliquet felis nec est varius eget auctor risus vestibulum. Nunc aliquet massa vel nibh pellentesque malesuada. Donec sed nisl sed odio pellentesque iaculis. Nam faucibus posuere augue, sit amet congue nunc dignissim at. Sed eu luctus orci. Aenean dapibus ligula in mi dapibus mattis. Nam faucibus mollis vulputate. Maecenas auctor pretium magna, eget suscipit mi fermentum quis. Phasellus tellus ante, eleifend sit amet sodales eget, feugiat vel lorem. Fusce id felis nunc. Donec urna nisl, consequat eget condimentum et, dapibus vel velit. Aenean eu massa quis diam euismod tempus eget ut mauris. In faucibus blandit leo, in luctus nulla porttitor id. Nullam eget felis et odio tempor aliquet. Quisque sed tristique ante. Pellentesque venenatis blandit sapien ut dictum. Mauris dapibus ligula ac mauris iaculis in pellentesque odio tincidunt.</p>
+			<p>Sed semper metus in ipsum fermentum porttitor. Donec fringilla nibh a nisi vehicula at venenatis est ultrices. Donec tempus turpis id quam pulvinar egestas eget sagittis turpis. Vestibulum ac lacus dolor, id lobortis eros. Nunc eu velit urna, vel bibendum ante. Integer mauris sem, tristique in luctus quis, varius eu est. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ipsum ipsum, sagittis vel interdum vitae, feugiat non orci. Aliquam erat volutpat. Donec rutrum velit eget leo adipiscing porttitor. In pulvinar, turpis quis suscipit molestie, turpis justo auctor justo, eu imperdiet lacus eros sit amet sapien. Donec ut diam est, eget varius libero. Morbi vitae tellus risus, vel fringilla sapien. Etiam tincidunt imperdiet volutpat. Donec hendrerit quam non leo molestie nec dignissim risus sollicitudin. Quisque eu mi risus.</p>
+		</div>
+	</div>
+	<div id="footer">
+		<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" /></a><br />Esta <span xmlns:dc="http://purl.org/dc/elements/1.1/" href="http://purl.org/dc/dcmitype/InteractiveResource" rel="dc:type">obra</span> est&#225; bajo una <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">licencia de Creative Commons</a> cc 2010 Makoa.mx<br />
+		Makoa.mx fue desarrollado durante un <strong>Super Happy Dev House</strong>
+	</div>
+  </body>
+</html>
