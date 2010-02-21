618a1112bca7cd0e79a2015fc78ddb48b3321bd2
Raul Pineda <shachiel@gmail.com>
2010-02-20 18:22:38 -0600

    Se agregaron las vistas de la pagina principal, css e imagenes necesaria

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> 
+  <head>
+	<link rel="stylesheet" type="text/css" href="stylesheets/style.css" />
+  </head>
+  <body>
+	<div id="header"><img src="images/header.png" alt="Makoa.mx" title="Makoa.mx" /></div><!--header.png es una imagen del logo que aún no está hecho-->
+	<div id="wrapper">
+		<div id="userInfo">Bienvenido @shachiel | <a class="logout" href="#">Cerrar Sesi&oacute;n</a></div> <!--@shachiel debe ser el username de twitter -->
+		<div id="appInfo">
+			<p>
+				<strong>Makoa.mx</strong> es un punto de reuni&oacute;n en el que puedes encontrar la ayuda que
+				necesitas para ese proyecto que te est&aacute; rompiendo la cabeza completamente gratis.	
+				Lo &uacute;nico que se te pide a cambio es que tu mismo ofrezcas tu ayuda para otros usuarios
+				en la misma situaci&oacute;n que t&uacute;.
+			</p>
+			<p>
+				<strong>Makoa</strong> es una palabra N&aacute;huatl que en espa&ntilde;ol significa <i>ayuda</i>
+			</p>
+		</div>
+		<div id="actionButtons">
+			<a href="#" id="" class="">
+				<img src="images/necesitomakoa.png" alt="Necesito Makoa"
+					title="Necesito Makoa" />
+			</a>
+			<a href="#" id="" class="">
+				<img src="images/ofrezcomakoa.png" alt="Ofrezco Makoa"
+					title="Ofrezco Makoa" />
+			</a>
+		</div>
+		<div id="makoaFeed">
+			<h2>Entradas recientes:</h2>
+			<div class="makoaPost">
+				<img src="" class="twttrAvatar" alt="" title="" />
+				<h3><span class="offer">Ofrezco</span>: Ayuda con proyecto de Ruby on Rails</h3>
+				<span class="twttrUser">@shachiel</span> <!--link al twitter del usuario-->
+			</div>
+			<button>Leer m&aacute;s</button>
+		</div>
+	</div>
+	<div id="footer">
+		<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" /></a><br />Esta <span xmlns:dc="http://purl.org/dc/elements/1.1/" href="http://purl.org/dc/dcmitype/InteractiveResource" rel="dc:type">obra</span> est&#225; bajo una <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">licencia de Creative Commons</a> cc 2010 Makoa.mx<br />
+		Makoa.mx fue desarrollado durante un <strong>Super Happy Dev House</strong>
+	</div>
+  </body>
+ </html>
